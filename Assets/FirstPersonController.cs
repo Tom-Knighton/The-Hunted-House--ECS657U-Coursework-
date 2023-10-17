ac97b39e0447c599098dc28ce34d9fa9cf269b40
Manjit Singh <ec211001@qmul.ac.uk>
2023-10-17 17:13:47 +0100

    Added sprinting

--- Assets/FirstPersonController.cs
+++ Assets/FirstPersonController.cs
@@ -6,11 +6,21 @@ using UnityEngine;
 public class FirstPersonController : MonoBehaviour
 {
     public bool CanMove { get; private set; } = true;
+    private bool IsSprinting => canSprint && Input.GetKey(sprintKey);
+
+    // Movement and control settings
+    [Header("Functional Options")]
+    [SerializeField] private bool canSprint = true;
+
+    [Header("Controls")]
+    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
 
     [Header("Movement Parameters")]
     [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float sprintSpeed = 6.0f;
     [SerializeField] private float gravity = 30.0f;
 
+    // Mouse look settings
     [Header("Look Parameters")]
     [SerializeField, Range(1, 10)] private float lookSpeedX = 2.0f;
     [SerializeField, Range(1, 10)] private float lookSpeedY = 2.0f;
@@ -25,7 +35,7 @@ public class FirstPersonController : MonoBehaviour
 
     private float rotationX = 0;
 
-    // Start is called before the first frame update
+    // Initialise components
     void Awake()
     {
         playerCamera = GetComponentInChildren<Camera>();
@@ -34,7 +44,7 @@ public class FirstPersonController : MonoBehaviour
         Cursor.visible = false;
     }
 
-    // Update is called once per frame
+    // Handle movement and look each frame
     void Update()
     {
         if (CanMove)
@@ -45,16 +55,23 @@ public class FirstPersonController : MonoBehaviour
             ApplyFinalMovements();
         }
     }
+
+    // Calculate player movement based on input
     private void HandleMovementInput()
     {
-        currentInput = new Vector2(walkSpeed * Input.GetAxis("Vertical"), walkSpeed * Input.GetAxis("Horizontal"));
+        currentInput = new Vector2((IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Vertical"), (IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Horizontal"));
 
+        // Calculates the move directions and makes sure vertical movement isn't affected.
         float moveDirectionY = moveDirection.y;
         moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) + (transform.TransformDirection(Vector3.right) * currentInput.y);
-        moveDirection = moveDirection.normalized * Mathf.Clamp(moveDirection.magnitude, 0, walkSpeed);
         moveDirection.y = moveDirectionY;
+
+        // Makes sure the player doesn't move faster diagonally.
+        float currentSpeed = IsSprinting ? sprintSpeed : walkSpeed;
+        moveDirection = moveDirection.normalized * Mathf.Clamp(moveDirection.magnitude, 0, currentSpeed);
     }
 
+    // Handle mouse look based on input
     private void HandleMouseLook()
     {
         rotationX -= Input.GetAxis("Mouse Y") * lookSpeedY;
@@ -63,6 +80,7 @@ public class FirstPersonController : MonoBehaviour
         transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeedX, 0);
     }
 
+    // Apply movement and gravity
     private void ApplyFinalMovements()
     {
         if (!characterController.isGrounded)
