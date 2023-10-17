88e2327e7108b08b77818e657c8c0469475b27ba
Manjit Singh <ec211001@qmul.ac.uk>
2023-10-17 17:32:54 +0100

    Added jumping

--- Assets/FirstPersonController.cs
+++ Assets/FirstPersonController.cs
@@ -1,24 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class FirstPersonController : MonoBehaviour
 {
     public bool CanMove { get; private set; } = true;
     private bool IsSprinting => canSprint && Input.GetKey(sprintKey);
+    private bool ShouldJump => Input.GetKeyDown(jumpKey) && characterController.isGrounded;
 
     // Movement and control settings
     [Header("Functional Options")]
     [SerializeField] private bool canSprint = true;
+    [SerializeField] private bool canJump = true;
 
     [Header("Controls")]
     [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
 
     [Header("Movement Parameters")]
     [SerializeField] private float walkSpeed = 3.0f;
     [SerializeField] private float sprintSpeed = 6.0f;
-    [SerializeField] private float gravity = 30.0f;
 
     // Mouse look settings
     [Header("Look Parameters")]
@@ -26,6 +27,10 @@ public class FirstPersonController : MonoBehaviour
     [SerializeField, Range(1, 10)] private float lookSpeedY = 2.0f;
     [SerializeField, Range(1, 180)] private float upperLookLimit = 80.0f;
     [SerializeField, Range(1, 180)] private float lowerLookLimit = 80.0f;
+
+    [Header("Jumping Parameters")]
+    [SerializeField] private float jumpForce = 8.0f;
+    [SerializeField] private float gravity = 30.0f;
 
     private Camera playerCamera;
     private CharacterController characterController;
@@ -35,7 +40,7 @@ public class FirstPersonController : MonoBehaviour
 
     private float rotationX = 0;
 
-    // Initialise components
+    // Awake is called when the script instance is being loaded
     void Awake()
     {
         playerCamera = GetComponentInChildren<Camera>();
@@ -44,13 +49,18 @@ public class FirstPersonController : MonoBehaviour
         Cursor.visible = false;
     }
 
-    // Handle movement and look each frame
+    // Update is called once per frame
     void Update()
     {
         if (CanMove)
         {
             HandleMovementInput();
             HandleMouseLook();
+
+            if (canJump)
+            {
+                HandleJump();
+            }
 
             ApplyFinalMovements();
         }
@@ -64,11 +74,14 @@ public class FirstPersonController : MonoBehaviour
         // Calculates the move directions and makes sure vertical movement isn't affected.
         float moveDirectionY = moveDirection.y;
         moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) + (transform.TransformDirection(Vector3.right) * currentInput.y);
-        moveDirection.y = moveDirectionY;
 
         // Makes sure the player doesn't move faster diagonally.
         float currentSpeed = IsSprinting ? sprintSpeed : walkSpeed;
         moveDirection = moveDirection.normalized * Mathf.Clamp(moveDirection.magnitude, 0, currentSpeed);
+
+        moveDirection.y = moveDirectionY;
+
+
     }
 
     // Handle mouse look based on input
@@ -78,6 +91,15 @@ public class FirstPersonController : MonoBehaviour
         rotationX = Mathf.Clamp(rotationX, -upperLookLimit, lowerLookLimit);
         playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
         transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeedX, 0);
+    }
+
+    // Handles jumping
+    private void HandleJump()
+    {
+        if (ShouldJump)
+        {
+            moveDirection.y = jumpForce;
+        }
     }
 
     // Apply movement and gravity
