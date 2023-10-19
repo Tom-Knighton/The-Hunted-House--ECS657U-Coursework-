5aa94b9461e5e558c45b87893fde220a2082993b
Tom K <tomknighton@icloud.com>
2023-10-19 21:54:45 +0100

    feat(enemy-ai): Basic 'vision' of player

--- Assets/Scripts/Enemy AI/Debugging/DebugFreeCam.cs
+++ Assets/Scripts/Enemy AI/Debugging/DebugFreeCam.cs
@@ -1,7 +1,7 @@
 using System;
 using UnityEngine;
 
-namespace Enemy_AI.Debug
+namespace Enemy_AI.Debugging
 {
     public class DebugFreeCam : MonoBehaviour
     {
--- Assets/Scripts/Enemy AI/Vision.cs
+++ Assets/Scripts/Enemy AI/Vision.cs
@@ -1,9 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-
 namespace Enemy_AI
 {
     public class Vision : MonoBehaviour
     {
+        public LayerMask PlayerLayerMask;
+        public LayerMask ObstacleMask;
         
+        private Collider[] _visionResults = new Collider[1];
+        
+        private void Update()
+        {
+            Physics.OverlapSphereNonAlloc(transform.position, 5f, _visionResults, PlayerLayerMask);
+            var player = _visionResults.FirstOrDefault();
+            if (player is null)
+            {
+                return;
+            }
+            
+            var forwardAngle = (player.transform.position - transform.position).normalized;
+            var distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+            if (!Physics.Raycast(transform.position, forwardAngle, distanceToPlayer, ObstacleMask))
+            {
+                Debug.DrawLine(transform.position, player.transform.position, Color.red);
+            }
+            else
+            {
+                Debug.Log("See but blocked");
+            }
+            
+        }
     }
 }
