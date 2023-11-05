18196b04c04efb2a4c69270272f2282437e4528c
Manjit Singh <manjitsd74@gmail.com>
2023-11-05 18:19:51 +0000

    Reformatted directories and added footstep tag to all floors

---
+++ Assets/Scripts/Menu/MainMenuController.cs
@@ -0,0 +1,37 @@
+using Game;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public GameObject instructionsPanel;
+    public void StartGame()
+    {
+        GameManager.debugMode = false;
+        SceneManager.LoadScene("MainGame");
+    }
+    public void OpenInstructions()
+    {
+        instructionsPanel.SetActive(true);
+    }
+
+    public void CloseInstructions()
+    {
+        instructionsPanel.SetActive(false);
+    }
+
+    public void RestartGame()
+    {
+        GameManager.debugMode = true;
+        UIManager.Instance.ShowPlayerUI();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameObject.SetActive(false);
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        gameObject.SetActive(false);
+    }
+}
