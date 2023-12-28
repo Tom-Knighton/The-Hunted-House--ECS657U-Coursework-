4fd8a929977492e2a6c7dbe8915b993d5604e27a
H4ssan1 <96207866+H4ssan1@users.noreply.github.com>
2023-12-28 17:16:52 +0000

    day/night cycle

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -7,7 +7,7 @@ namespace Game
 {
     public class GameManager: MonoBehaviour
     {
-        private DateTime _gameEndTime = DateTime.Parse("2023-01-02 08:00:00"); // 8am the next day
+        private DateTime _gameEndTime = DateTime.Parse("2023-01-02 06:00:00"); // 8am the next day
         private DateTime _currentTime;
 
         [SerializeField] private FirstPersonController player;
@@ -36,10 +36,24 @@ namespace Game
             {
                 enemy.gameObject.SetActive(false);
             }
+<<<<<<< Updated upstream
             
             UIManager.Instance.ShowOpeningScrawl();
 
             _currentTime = DateTime.Parse("2023-01-01 18:00:00"); // Set time to 6pm so we can get roughly a day night cycle
+=======
+            else
+            {
+                // Disable player and enemies for non-debug mode
+                player.enabled = false;
+                foreach (var enemy in enemies)
+                {
+                    enemy.gameObject.SetActive(false);
+                }
+                UIManager.Instance.ShowOpeningScrawl();  // Show the opening narrative
+            }
+            _currentTime = DateTime.Parse("2023-01-01 20:00:00");
+>>>>>>> Stashed changes
         }
 
         private void Update()
@@ -49,7 +63,13 @@ namespace Game
 
         private void UpdateTime()
         {
+<<<<<<< Updated upstream
             _currentTime = _currentTime.AddMilliseconds(Time.deltaTime * 10000f);
+=======
+            // Advance the current time by a scaled amount of real time
+            _currentTime = _currentTime.AddSeconds(Time.deltaTime * 60f);
+            // Update the UI with the time left until the game ends
+>>>>>>> Stashed changes
             UIManager.Instance.UpdateTimeLeft(_gameEndTime - _currentTime);
             
             if (_currentTime >= _gameEndTime)
