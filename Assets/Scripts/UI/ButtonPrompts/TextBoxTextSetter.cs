75f48faa1670a73fcd1be014d66eb03a50c0b457
Manjit Singh <manjitsd74@gmail.com>
2024-01-07 03:57:37 +0000

    Button prompts work with rebinds
    FINALLY managed to get the button prompts to work with the rebinds after working on it for 12 hours straight :sob: Made it so that it compares the playerRef to the default input system, that way you can quite easily change keybinds and have them pop up whilst also maintaining button prompts for controller too.

--- Assets/Scripts/UI/ButtonPrompts/TextBoxTextSetter.cs
+++ Assets/Scripts/UI/ButtonPrompts/TextBoxTextSetter.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.DualShock;
+using System.Collections.Generic;
+using System;
+using System.Linq;
 
 public class TextBoxTextSetter : MonoBehaviour
 {
@@ -10,16 +13,34 @@ public class TextBoxTextSetter : MonoBehaviour
 
     [SerializeField] private SpriteAssetsList spriteAssetsList;
 
+    [Serializable]
+    public class InputBindingContainer
+    {
+        public List<InputBinding> bindings;
+    }
+
     private PlayerInputActions _playerInput;
+    private PlayerInput _playerInputComponent;
     private TMP_Text _textBox;
+
 
     private void Awake()
     {
         _playerInput = new PlayerInputActions();
         _textBox = GetComponent<TMP_Text>();
+        _playerInputComponent = GetComponent<PlayerInput>();
+
+        LoadBindingOverrides();
+        _playerInput.Gameplay.Enable();
+    }
+
+    private void LoadBindingOverrides()
+    {
         string rebinds = PlayerPrefs.GetString("rebinds", string.Empty);
         if (!string.IsNullOrEmpty(rebinds))
+        {
             _playerInput.LoadBindingOverridesFromJson(rebinds);
+        }
     }
 
     private void Start()
@@ -29,44 +50,99 @@ public class TextBoxTextSetter : MonoBehaviour
 
     public void SetInteractText()
     {
+        UpdateBinding();
         SetText();
     }
+
+    private void UpdateBinding()
+    {
+
+        // Get the current control scheme from the PlayerInput component
+        var controlScheme = _playerInputComponent.currentControlScheme;
+
+        // Find the binding index for the current control scheme
+        var bindingIndex = _playerInput.Gameplay.Interact.bindings.ToList().FindIndex(
+            binding => binding.groups != null && binding.groups.Contains(controlScheme)
+        );
+
+        // If a valid binding index is found, update the message
+        if (bindingIndex != -1)
+        {
+            var binding = _playerInput.Gameplay.Interact.bindings[bindingIndex];
+            var interactKey = InputControlPath.ToHumanReadableString(binding.effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice);
+            message = $"Press {interactKey} to interact";
+        }
+        else
+        {
+            Debug.LogWarning("No valid binding found for the current control scheme.", this);
+        }
+    }
+
+    private bool AreKeybindsChanged()
+    {
+        // Create a new instance to get the default keybinds
+        var defaultActions = new PlayerInputActions();
+        var defaultKeybinds = defaultActions.Gameplay.Interact.bindings.Select(b => b.effectivePath).ToList();
+
+        // Compare with current keybinds
+        var currentKeybinds = _playerInput.Gameplay.Interact.bindings.Select(b => b.effectivePath).ToList();
+
+        // Check if the keybinds are different
+        bool keybindsAreDifferent = !defaultKeybinds.SequenceEqual(currentKeybinds);
+        return keybindsAreDifferent;
+    }
+
+
 
     private void SetText()
     {
         var currentDevice = InputSystem.GetDevice<InputDevice>();
-
         TMP_SpriteAsset spriteAsset = DetermineSpriteAsset(currentDevice);
         var bindingIndex = currentDevice is Keyboard || currentDevice is Mouse ? 0 : 1;
+        bool keybindsChanged = AreKeybindsChanged();
+
+        InputBinding binding;
+        if (keybindsChanged)
+        {
+            binding = _playerInput.Gameplay.Interact.bindings[bindingIndex];
+        }
+        else
+        {
+            binding = new PlayerInputActions().Gameplay.Interact.bindings[bindingIndex];
+        }
 
         _textBox.text = TextButtonPromptSprite.ReadAndReplaceBinding(
             message,
-            _playerInput.Gameplay.Interact.bindings[bindingIndex],
+            binding,
             spriteAsset
         );
     }
 
+
     private TMP_SpriteAsset DetermineSpriteAsset(InputDevice currentDevice)
     {
+        TMP_SpriteAsset selectedSpriteAsset;
         if (currentDevice is Keyboard || currentDevice is Mouse)
         {
-            return spriteAssetsList.SpriteAssets[0];
+            selectedSpriteAsset = spriteAssetsList.SpriteAssets[0];
         }
         else if (currentDevice is Gamepad gamepad)
         {
             if (gamepad is DualShockGamepad || gamepad is DualSenseGamepadHID)
             {
-                return spriteAssetsList.SpriteAssets[1];
+                selectedSpriteAsset = spriteAssetsList.SpriteAssets[1];
             }
             else
             {
-                return spriteAssetsList.SpriteAssets[2];
+                selectedSpriteAsset = spriteAssetsList.SpriteAssets[2];
             }
         }
         else
         {
             Debug.LogWarning($"Unrecognized input device: {currentDevice}. Defaulting to Keyboard sprites.");
-            return spriteAssetsList.SpriteAssets[0];
+            selectedSpriteAsset = spriteAssetsList.SpriteAssets[0];
         }
+        return selectedSpriteAsset;
     }
+
 }
